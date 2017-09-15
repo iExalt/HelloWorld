f6703fe8aedf808cbdcffecffca0045b8f0d1709
Clement Liaw <cklliaw@edu.uwaterloo>
2017-09-15 19:27:59 -0400

    Activate all LEDs in series

--- helloworld/helloworld.v
+++ helloworld/helloworld.v
@@ -1,11 +1,17 @@
-module helloworld (clk, led);	
+module helloworld (clk, led, key0, key1);	
 		input clk;
+		input key0;
+		input key1;
 		output reg [7:0] led; 
 	
 
 
 reg [31:0] counter;
 reg [7:0] i;
+//assign i = 0;
+//assign led[0] = 1;
+
+
 
 always @ (posedge clk)
 begin
@@ -16,11 +22,31 @@ begin
 
 	else
 	begin
-	counter <= 0;
-		for (i = 0; i <= 7; i = i + 1)
+	counter <= 0;	
+	if (i == 0 & led[0] == 0)
+	begin
+		led[0] <= 1;
+	end
+	
+		if (key0 | key1)
 		begin
+			if (i < 7)
+			begin
+				led[i] = 0;
+				i = i + 1;
+				led[i] = 1;
+			end
 			
-			led[i] <= ~led[i];
+			else
+			begin
+				i <= 0;
+				led[7] = 0;
+				led[0] = 1;
+			end
+		end
+		else 
+		begin
+			led[i] = ~led[i];	
 		end
 	end
 	
