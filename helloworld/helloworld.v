d5ae2d2d7a67d9dfe7f424a32b25e0fcccb84d8e
Clement Liaw <cklliaw@edu.uwaterloo>
2017-10-02 11:38:59 -0400

    Switches change toggled LED

--- helloworld/helloworld.v
+++ helloworld/helloworld.v
@@ -1,17 +1,23 @@
-module helloworld (clk, led, key0, key1);	
+module helloworld (clk, led, dipswitch, key0, key1);
+		// declare inputs
+		// pins are in pin planner/DE0-Nano User Guide
 		input clk;
 		input key0;
 		input key1;
+		input [3:0] dipswitch;
+		
 		output reg [7:0] led; 
 	
 
 
-reg [31:0] counter;
-reg [7:0] i;
-
+reg [31:0] counter = 0;
+integer i = 0;
+reg [3:0] toggle = 0;
 
 always @ (posedge clk)
 begin
+	// 50MHz internal clock
+	// 25M clock cycles = 0.5s
 	if (counter <= 25000000)
 	begin
 		counter <= counter + 1;
@@ -19,32 +25,85 @@ begin
 
 	else
 	begin
-	counter <= 0;	
-	if (i == 0 & led[0] == 0)
-	begin
-		led[0] <= 1;
-	end
+		counter <= 0;	
 	
-		if (~key0 | ~key1)
+		// increment or decrement on buttonpress depending on button pressed
+		if (~key0)
 		begin
-			if (i < 7)
+			led[i] = 0;
+			if (dipswitch[0] == 1)
 			begin
-				led[i] = 0;
-				i = i + 1;
-				led[i] = 1;
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[1] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[2] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[3] == 1)
+			begin
+				toggle = toggle + 1'b1;
 			end
 			
-			else
+			// increment i by the number of switches activated
+			i = i + toggle;
+			
+			// wraparound to 0
+			if (i > 7)
 			begin
-				i <= 0;
-				led[7] = 0;
-				led[0] = 1;
+				i = i - 8;
 			end
+			
+			led[i] = 1;
+		
 		end
+		else if (~key1)
+		begin
+			led[i] = 0;
+			if (dipswitch[0] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[1] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[2] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			if (dipswitch[3] == 1)
+			begin
+				toggle = toggle + 1'b1;
+			end
+			
+			// decrement i by the number of switches activated
+			i = i - toggle;
+			
+			// wraparound to 7
+			if (i < 0)
+			begin
+				i = i + 8;
+			end
+			
+			// activate selected LED
+			led[i] = 1;
+		
+		end
+
 		else 
 		begin
-			led[i] = ~led[i];	
+	
 		end
+		
+		// flash LED
+		led[i] = ~led[i];	
+		
+		// reset toggle value
+		toggle = 0;
 	end
 	
 	
